3f4916404d001892ef0fc4a2cbd4399145ab1c53
Justin Sung <jsung7@iastate.edu>
2019-03-25 19:37:06 -0500

    added emax

--- src/RBTree.java
+++ src/RBTree.java
@@ -3,11 +3,11 @@
  * @author Lauren Arner
  * @author Richard Smith
  * @author Justin Sung
- * 
+ *
  * RBTree class, maintains operations on RBTree.
  */
 public class RBTree {
-	
+
 	/**
 	 * RB Tree constructor. It initializes nil node as well.
 	 */
@@ -15,28 +15,28 @@ public class RBTree {
 	Node NIL;
 	int nodeCount;
 	int height;
-	
+
 	public RBTree() {
 		//TODO: Add code as needed.
-		
+
 		this.NIL = new Node();
 		this.NIL.color = 1;
 		this.root = this.NIL;
 		NIL.parent = NIL;
 		NIL.left = NIL;
 		NIL.right = NIL;
-		
+
 	}
-	
+
 	/**
 	 * Returns the root of teh tree.
 	 * @return
 	 */
 	public Node getRoot() {
-		
+
 		return this.root;
 	}
-	
+
 	/**
 	 * Returns reference for the nil node, for the rbTree.
 	 * @return
@@ -44,7 +44,7 @@ public class RBTree {
 	public Node getNILNode() {
 		return this.NIL;
 	}
-	
+
 	/**
 	 * Returns the number of internal nodes in the tree.
 	 * @return
@@ -52,8 +52,8 @@ public class RBTree {
 	public int getSize() {
 		return this.nodeCount;
 	}
-	
-	
+
+
 	/**
 	 * Returns the height of the tree.
 	 * @return
@@ -61,33 +61,42 @@ public class RBTree {
 	public int getHeight() {
 		return this.height;
 	}
-	
+
 	//Add more functions as  you see fit.
-	
+
 	/**
 	 * Computes value variable for nodes recursively
 	 * @param node
 	 * @return
 	 */
 	public int computeVal(Node node) {
-		
+
 		if(node == NIL) {
 			return 0;
 		}
-		
-		return computeVal(node.left) + node.p + computeVal(node.right); 
-		
+
+		return computeVal(node.left) + node.p + computeVal(node.right);
+
 	}
-	
+
 	/**
-	 * Computes max value variable for nodes 
+	 * Computes max value variable for nodes
 	 * @param node
 	 */
-	
+
 	public void computeMaxVal(Node node) {
-			
+
 		node.maxval = max(node.left.getMaxVal(), node.right.getMaxVal() + node.getP(), node.left.getVal() + node.getP() + node.right.getMaxVal());
-		
+
+		if (node.maxval == node.left.getMaxVal()){
+			node.emax = node.left.getEmax();
+		}
+		else if (node.maxval == node.right.getMaxVal() + node.getP()) {
+			node.emax = node.getEndpoint();
+		}
+		else {
+			node.emax = node.right.getEmax();
+		}
 	}
 
 	/**
@@ -99,7 +108,7 @@ public class RBTree {
 	 */
 	private int max(int first, int second, int third) {
 		int max;
-		
+
 		if(first >= second && first >= third) {
 			max = first;
 		}else if(second >= first && second >= third) {
@@ -109,6 +118,6 @@ public class RBTree {
 		}
 		return max;
 	}
-	
-	
+
+
 }
