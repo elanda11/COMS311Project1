d4376bd6ac37a6c53d648769423c45bbf455ef03
Justin Sung <jsung7@iastate.edu>
2019-03-30 13:39:12 -0500

    updateed RBOperationsTest

--- src/RBOperationsTest.java
+++ src/RBOperationsTest.java
@@ -1,184 +1,139 @@
-import org.junit.Before;
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+  import org.junit.Before; import org.junit.Test;
 
-public class RBOperationsTest {
+  import static org.junit.Assert.*;
 
-    // Instance variables
-    RBTree tree;
-    Node node1;
-    Node node2;
-    Node node3;
-    Node node4;
+  public class RBOperationsTest {
 
-    @Before
-    public void initialize() {
-        tree = new RBTree();
-        node1 = new Node(new Endpoint(1, 1));
-        node2 = new Node(new Endpoint(2, 1));
-        node3 = new Node(new Endpoint(3, 1));
-        node4 = new Node(new Endpoint(4, 1));
-    }
+  // Instance variables RBTree tree; Node node1; Node node2; Node node3; Node
+  node4; Node node5; Node node6; Node node7; Node node8;
 
-    @Test
-    public void firstInsert() {
-        tree.insert(node1);
-        assertEquals(tree.getRoot().getKey(), 1);
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
-    }
+  @Before public void initialize() { tree = new RBTree(); node4 = new Node();
+  node2 = new Node(); node7 = new Node(); node1 = new Node(); node3 = new
+  Node(); node5 = new Node(); node8 = new Node(); node6 = new Node();
 
-    @Test
-    public void leftInsert() {
-        tree.insert(node2);
-        tree.insert(node1);
-        assertEquals(tree.getRoot().getKey(), 2);
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
-        assertEquals(tree.getRoot().getLeft().getKey(), 1);
-        assertEquals(tree.getRoot().getLeft().getColorInEnum(), Color.RED);
-        assertTrue(tree.getRoot().getRight().isNil());
-    }
+  node4.parent = tree.NIL; node4.left = node2; node4.right = node7; node4.key =
+  4; node4.color = 1; node4.val = 0; node4.p = -1; node4.maxval = 3;
+  //node.emax is fucked
 
-    @Test
-    public void rightInsert() {
-        tree.insert(node2);
-        tree.insert(node3);
-        assertEquals(tree.getRoot().getKey(), 2);
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
-        assertEquals(tree.getRoot().getRight().getKey(), 3);
-        assertEquals(tree.getRoot().getRight().getColorInEnum(), Color.RED);
-        assertTrue(tree.getRoot().getLeft().isNil());
-    }
+  node2.parent = node4; node2.left = node1; node2.right = node3; node2.key = 1;
+  node2.color = 0; node2.val = 3; node2.p = 1; node2.maxval = 3;
 
-    @Test
-    public void leftRightInsert() {
+  node7.parent = node4; node7.left = node5; node7.right = node8; node7.key = 9;
+  node7.color = 0; node7.val = -2; node7.p = -1; node7.maxval = 0;
 
-        // Build tree
-        tree.insert(node2);
-        tree.insert(node1);
-        tree.insert(node3);
+  node1.parent = node2; node1.left = tree.NIL; node1.right = tree.NIL;
+  node1.key = 0; node1.color = 1; node1.val = 1; node1.p = 1; node1.maxval = 1;
 
-        // Test initial insertion
-        assertEquals(tree.getRoot().getKey(), node2.getKey());
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
+  node3.parent = node2; node3.left = tree.NIL; node3.right = tree.NIL;
+  node3.key = 3; node3.color = 1; node3.val = 1; node3.p = 1; node3.maxval = 1;
 
-        // Test root's children
-        Node left = tree.getRoot().getLeft();
-        Node right = tree.getRoot().getRight();
-        assertEquals(left.getKey(), node1.getKey());
-        assertEquals(right.getKey(), node3.getKey());
-        assertEquals(left.getParent().getKey(), node2.getKey());
-        assertEquals(right.getParent().getKey(), node2.getKey());
-        assertEquals(left.getColorInEnum(), Color.RED);
-        assertEquals(right.getColorInEnum(), Color.RED);
+  node5.parent = node7; node5.left = tree.NIL; node5.right = tree.NIL;
+  node5.key = 6; node5.color = 1; node5.val = 0; node5.p = -1; node5.maxval =
+  0;
 
-        // Test children's children are nil
-        assertTrue(left.getLeft().isNil());
-        assertTrue(left.getRight().isNil());
-        assertTrue(right.getLeft().isNil());
-        assertTrue(right.getRight().isNil());
+  node8.parent = node7; node8.left = tree.NIL; node8.right = tree.NIL;
+  node8.key = 11; node8.color = 1; node8.val = -1; node8.p = -1; node8.maxval =
+  0;
 
-    }
+  node1.left = tree.NIL; node1.right = tree.NIL; node6.key = 7; node6.color =
+  0; node6.val = 1; node6.p = 1; node6.maxval = 1; }
 
-    @Test
-    public void insertWithLeftRotation() {
+  @Test public void firstInsert() { tree.RBInsert(node1);
+  assertEquals(tree.getRoot().getKey(), 4);
+  assertEquals(tree.getRoot().getColor(), 0); }
 
-        // Build tree
-        tree.insert(node1);
-        tree.insert(node2);
-        tree.insert(node3);
+  @Test public void leftInsert() { tree.RBInsert(node2); tree.RBInsert(node1);
+  assertEquals(tree.getRoot().getKey(), 2);
+  assertEquals(tree.getRoot().getColor(), 1);
+  assertEquals(tree.getRoot().getLeft().getKey(), 0);
+  assertEquals(tree.getRoot().getLeft().getColor(), 1);
 
-        // Test root after rotation
-        assertEquals(tree.getRoot().getKey(), 2);
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
+  }
 
-        // Test children
-        Node left = tree.getRoot().getLeft();
-        Node right = tree.getRoot().getRight();
-        assertEquals(left.getKey(), 1);
-        assertEquals(right.getKey(), 3);
-        assertEquals(left.getParent(), tree.getRoot());
-        assertEquals(right.getParent(), tree.getRoot());
-        assertEquals(left.getColorInEnum(), Color.RED);
-        assertEquals(right.getColorInEnum(), Color.RED);
+  @Test public void rightInsert() { tree.RBInsert(node2); tree.RBInsert(node3);
+  assertEquals(tree.getRoot().getKey(), 2);
+  assertEquals(tree.getRoot().getColor(), 1);
+  assertEquals(tree.getRoot().getRight().getKey(), 3);
+  assertEquals(tree.getRoot().getRight().getColor(), 1);
 
-        // Test grandchildren are nil
-        assertTrue(left.getLeft().isNil());
-        assertTrue(left.getRight().isNil());
-        assertTrue(right.getLeft().isNil());
-        assertTrue(right.getRight().isNil());
-    }
+  }
 
-    @Test
-    public void insertWithRightRotation() {
+  @Test public void leftRightInsert() {
 
-        // Build tree
-        tree.insert(node3);
-        tree.insert(node2);
-        tree.insert(node1);
+  // Build tree tree.RBInsert(node2); tree.RBInsert(node1);
+  tree.RBInsert(node3);
 
-        // Test root after rotation
-        assertEquals(tree.getRoot().getKey(), 2);
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
+  // Test initial insertion assertEquals(tree.getRoot().getKey(),
+  node2.getKey()); assertEquals(tree.getRoot().getColor(), 0);
 
-        // Test children
-        Node left = tree.getRoot().getLeft();
-        Node right = tree.getRoot().getRight();
-        assertEquals(left.getKey(), 1);
-        assertEquals(right.getKey(), 3);
-        assertEquals(left.getParent(), tree.getRoot());
-        assertEquals(right.getParent(), tree.getRoot());
-        assertEquals(left.getColorInEnum(), Color.RED);
-        assertEquals(right.getColorInEnum(), Color.RED);
+  // Test root's children Node left = tree.getRoot().getLeft(); Node right =
+  tree.getRoot().getRight(); assertEquals(left.getKey(), node1.getKey());
+  assertEquals(right.getKey(), node3.getKey());
+  assertEquals(left.getParent().getKey(), node2.getKey());
+  assertEquals(right.getParent().getKey(), node2.getKey());
+  assertEquals(left.getColor(), 1); assertEquals(right.getColor(), 1);
 
-        // Test grandchildren are nil
-        assertTrue(left.getLeft().isNil());
-        assertTrue(left.getRight().isNil());
-        assertTrue(right.getLeft().isNil());
-        assertTrue(right.getRight().isNil());
-    }
 
-    @Test
-    public void insertCase3() {
 
-        // Build tree to create case 3 scenario (from Wikipedia)
-        tree.insert(node3);
-        tree.insert(node2);
-        tree.insert(node4);
-        tree.insert(node1);
+  }
 
-        // Test if colors were repainted correctly
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
-        assertEquals(tree.getRoot().getLeft().getColorInEnum(), Color.BLACK);
-        assertEquals(tree.getRoot().getRight().getColorInEnum(), Color.BLACK);
-        assertEquals(tree.getRoot().getLeft().getLeft().getColorInEnum(), Color.RED);
-    }
+  @Test public void insertWithLeftRotation() {
 
-    @Test
-    public void insertCase4() {
+  // Build tree tree.RBInsert(node1); tree.RBInsert(node2);
+  tree.RBInsert(node3);
 
-        // Build tree to create case 4 scenario (from Wikipedia)
-        tree.insert(node3);
-        tree.insert(node1);
-        tree.insert(node4);
-        tree.getRoot().getRight().setColor(Color.BLACK);    // Set color of uncle to black
-        tree.insert(node2);
+  // Test root after rotation assertEquals(tree.getRoot().getKey(), 2);
+  assertEquals(tree.getRoot().getColor(), 0);
 
-        // Test rotations
-        assertEquals(tree.getRoot().getKey(), 2);
-        assertEquals(tree.getRoot().getLeft().getKey(), 1);
-        assertEquals(tree.getRoot().getRight().getKey(), 3);
-        assertEquals(tree.getRoot().getRight().getRight().getKey(), 4);
-        assertTrue(tree.getRoot().getLeft().getLeft().isNil());
-        assertTrue(tree.getRoot().getLeft().getRight().isNil());
-        assertTrue(tree.getRoot().getRight().getLeft().isNil());
-        assertTrue(tree.getRoot().getRight().getRight().getLeft().isNil());
-        assertTrue(tree.getRoot().getRight().getRight().getRight().isNil());
+  // Test children Node left = tree.getRoot().getLeft(); Node right =
+  tree.getRoot().getRight(); assertEquals(left.getKey(), 1);
+  assertEquals(right.getKey(), 3); assertEquals(left.getParent(),
+  tree.getRoot()); assertEquals(right.getParent(), tree.getRoot());
+  assertEquals(left.getColor(), 1); assertEquals(right.getColor(), 1);
 
-        // Test colors
-        assertEquals(tree.getRoot().getColorInEnum(), Color.BLACK);
-        assertEquals(tree.getRoot().getLeft().getColorInEnum(), Color.RED);
-        assertEquals(tree.getRoot().getRight().getColorInEnum(), Color.RED);
-        assertEquals(tree.getRoot().getRight().getRight().getColorInEnum(), Color.BLACK);
-    }
-}
+
+  }
+
+  @Test public void insertWithRightRotation() {
+
+  // Build tree tree.RBInsert(node3); tree.RBInsert(node2);
+  tree.RBInsert(node1);
+
+  // Test root after rotation assertEquals(tree.getRoot().getKey(), 2);
+  assertEquals(tree.getRoot().getColor(), 0);
+
+  // Test children Node left = tree.getRoot().getLeft(); Node right =
+  tree.getRoot().getRight(); assertEquals(left.getKey(), 1);
+  assertEquals(right.getKey(), 3); assertEquals(left.getParent(),
+  tree.getRoot()); assertEquals(right.getParent(), tree.getRoot());
+  assertEquals(left.getColor(), 1); assertEquals(right.getColor(), 1); }
+
+  @Test public void insertCase3() {
+
+  // Build tree to create case 3 scenario (from Wikipedia)
+  tree.RBInsert(node3); tree.RBInsert(node2); tree.RBInsert(node4);
+  tree.RBInsert(node1);
+
+  // Test if colors were repainted correctly
+  assertEquals(tree.getRoot().getColor(), 0);
+  assertEquals(tree.getRoot().getLeft().getColor(), 0);
+  assertEquals(tree.getRoot().getRight().getColor(), 0);
+  assertEquals(tree.getRoot().getLeft().getLeft().getColor(), 1); }
+
+  @Test public void insertCase4() {
+
+  // Build tree to create case 4 scenario (from Wikipedia)
+  tree.RBInsert(node3); tree.RBInsert(node1); tree.RBInsert(node4);
+  tree.RBInsert(node2);
+
+  // Test rotations assertEquals(tree.getRoot().getKey(), 2);
+  assertEquals(tree.getRoot().getLeft().getKey(), 1);
+  assertEquals(tree.getRoot().getRight().getKey(), 3);
+  assertEquals(tree.getRoot().getRight().getRight().getKey(), 4);
+
+
+  // Test colors assertEquals(tree.getRoot().getColor(), 1);
+  assertEquals(tree.getRoot().getLeft().getColor(), 1);
+  assertEquals(tree.getRoot().getRight().getColor(), 1);
+  assertEquals(tree.getRoot().getRight().getRight().getColor(), 0); } }
